7876227a9289075661140ebba7f617a81cacde5e
Amananand <romaniohri@192.168.1.4>
2016-07-24 00:47:12 -0400

    One Change

--- HelloGit/src/com/hg/test/HelloTest.java
+++ HelloGit/src/com/hg/test/HelloTest.java
@@ -9,11 +9,8 @@ public class HelloTest {
 		System.out.println("Hello First Git:");
 		System.out.println(" Second commit");
 		System.out.println(" commit 1");
-<<<<<<< HEAD
+		System.out.println(" change");
 		
-=======
-		System.out.println(" commit 2")
->>>>>>> 9eb9128afc451782703a8c1cf6f202cb5850f3e1
 	}
 
 }
